7df6c5e76b50080153923279ee91a59d52afd94f
Nurmukhammed Duisekeyev <nureke92@gmail.com>
2022-01-01 17:10:57 +0600

    Fix posts since it's not SEO friendly, pre is empty []

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -8,14 +8,14 @@ const Posts: FunctionComponent<PostsProps> = () => {
   const [posts, setPosts] = useState([]);
 
   // Without using SSR, since <pre>[]</pre> - an empty array
-  useEffect(() => {
-    const load = async () => {
-      const response = await fetch("http://localhost:4200/posts");
-      const json = await response.json();
-      setPosts(json);
-    };
-    load();
-  }, []);
+  // useEffect(() => {
+  //   const load = async () => {
+  //     const response = await fetch("http://localhost:4200/posts");
+  //     const json = await response.json();
+  //     setPosts(json);
+  //   };
+  //   load();
+  // }, []);
 
   const linkHandler = () => {
     Router.push("/");
@@ -25,7 +25,7 @@ const Posts: FunctionComponent<PostsProps> = () => {
     <MainLayout title={"Posts"}>
       <h1>Posts</h1>
       <button onClick={linkHandler}>Go Back to Home</button>
-      <pre>{JSON.stringify(posts, null, 2)}</pre>
+      <pre>{JSON.stringify([], null, 2)}</pre>
     </MainLayout>
   );
 };
