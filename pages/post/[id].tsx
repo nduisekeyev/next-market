d915f291d8219b8b28cd7a70e8cf9ab930f4762f
Nurmukhammed Duisekeyev <nureke92@gmail.com>
2022-01-01 20:45:24 +0600

    Add SSR for posts using getInitialProps

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,8 +1,62 @@
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
 import { useRouter } from "next/router";
-import React, { FunctionComponent } from "react";
+import MainLayout from "../../components/MainLayout";
 
-export default function Post() {
-  const router = useRouter();
-  console.log("router", router);
-  return <h1>Post {router.query.id}</h1>;
+interface PostProps {
+  post: PostServerProps;
 }
+
+interface PostServerProps {
+  id: number;
+  title: string;
+  body: string;
+}
+
+export default function Post({ post: serverPost }: PostProps) {
+  const [post, setPost] = useState(serverPost);
+  const router = useRouter();
+
+  useEffect(() => {
+    const load = async () => {
+      const response = await fetch(
+        `http://localhost:4200/posts/${router.query.id}`
+      );
+      const data = await response.json();
+      console.log("data", data);
+      setPost(data);
+    };
+
+    if (!serverPost) {
+      load();
+    }
+  }, [router.query.id, serverPost]);
+
+  return (
+    <>
+      {!post ? (
+        <MainLayout title={"Loading"}>Loading...</MainLayout>
+      ) : (
+        <MainLayout title={"Post"}>
+          <h1>Post {post.id}</h1>
+          <p>{post.title}</p>
+          <p>{post.body}</p>
+          <Link href={"/posts"}>
+            <a>Back to all posts</a>
+          </Link>
+        </MainLayout>
+      )}
+    </>
+  );
+}
+
+Post.getInitialProps = async ({ query, req }: any) => {
+  if (!req) {
+    return { post: null }; // if there is no post from server return null
+  }
+  const response = await fetch(`http://localhost:4200/posts/${query.id}`);
+  const post = await response.json();
+  console.log("post", post);
+
+  return { post };
+};
--- pages/posts.tsx
+++ pages/posts.tsx
@@ -1,40 +1,60 @@
 import React, { FunctionComponent, useState, useEffect } from "react";
+import Link from "next/link";
 import Router from "next/router";
 import MainLayout from "../components/MainLayout";
 
 interface PostsProps {
-  posts: object;
+  posts: any;
 }
 
-const Posts = ({ posts }: PostsProps) => {
-  // const [posts, setPosts] = useState([]);
+const Posts = ({ posts: serverPosts }: PostsProps) => {
+  const [posts, setPosts] = useState(serverPosts);
 
-  // Without using SSR, since <pre>[]</pre> - an empty array
-  // useEffect(() => {
-  //   const load = async () => {
-  //     const response = await fetch("http://localhost:4200/posts");
-  //     const json = await response.json();
-  //     setPosts(json);
-  //   };
-  //   load();
-  // }, []);
+  useEffect(() => {
+    const load = async () => {
+      const response = await fetch("http://localhost:4200/posts");
+      const json = await response.json();
+      setPosts(json);
+    };
+    if (!posts) {
+      load();
+    }
+  }, [posts]);
 
   const linkHandler = () => {
     Router.push("/");
   };
 
   return (
-    <MainLayout title={"Posts"}>
-      <h1>Posts</h1>
-      <button onClick={linkHandler}>Go Back to Home</button>
-      <pre>{JSON.stringify(posts, null, 2)}</pre>
-    </MainLayout>
+    <>
+      {!posts ? (
+        <MainLayout title={"Loading"}>Loading...</MainLayout>
+      ) : (
+        <MainLayout title={"Posts"}>
+          <h1>Posts</h1>
+          <button onClick={linkHandler}>Go Back to Home</button>
+
+          <ul>
+            {posts.map((post: any) => (
+              <li key={post.id}>
+                <Link href={`/post/[id]`} as={`/post/${post.id}`}>
+                  <a>{post.title}</a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </MainLayout>
+      )}
+    </>
   );
 };
 
 export default Posts;
 
-Posts.getInitialProps = async () => {
+Posts.getInitialProps = async ({ req }: any) => {
+  if (!req) {
+    return { posts: null };
+  }
   const response = await fetch("http://localhost:4200/posts");
   const posts = await response.json();
 
