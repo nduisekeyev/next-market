9cf749ee0cb66615c8e31200bea1397bb4724c50
Nur Duisekeyev <nurmukhammedd@unl.solutions>
2022-05-17 17:50:37 +0600

    feat: add ngprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,22 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
-import NextNprogress from "nextjs-progressbar";
+
+import NProgress from "nprogress";
+import "../styles/nprogress.scss";
+
+import Router from "next/router";
+
+Router.events.on("routeChangeStart", (url) => {
+  console.log(`Loading: ${url}`);
+  NProgress.start();
+});
+
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <NextNprogress
-        color="yellow"
-        startPosition={0.3}
-        stopDelayMs={200}
-        height={3}
-        showOnShallow={true}
-      />
       <Component {...pageProps} />
       {/* <style jsx global>{`
         body {
